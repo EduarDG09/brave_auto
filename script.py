b52a155637cadb73396769d427016d4c729645f3
Eduardo <eduardgm201@gmail.com>
2021-12-08 12:47:15 -0400

    Added opening browser function

--- script.py
+++ script.py
@@ -1,3 +1,43 @@
 # Script para automatizar Brave Browser --
-print("Hello World")
+
+# imports 
+import webbrowser, time, os, random
+
+# includes
+webs = ["https://2captcha.com/",
+"https://forobeta.com/",
+"https://raidforums.com/",
+"https://www.blackhatworld.com/",
+"https://foro-ptc.com/index.php",
+"https://www.idle-empire.com/",
+"https://eurekasurveys.com/",
+"https://surveys.gobranded.com/",
+"https://cointiply.com/",
+"https://forobeta.com/temas/de-regreso-a-espana-ayuda.867882/page-2#post-7514457",
+"https://forobeta.com/temas/me-compre-una-linda-note-n_n.867917/#post-7514451",
+"https://forobeta.com/temas/buenas-alguien-que-me-ayude-tengo-este-problema.867918/#post-7514448",
+"https://forobeta.com/temas/quien-me-ayuda-a-pagar-el-vip.867909/#post-7514363",
+"https://vendercomprardolares.com/calculadora-comisiones-paypal.php",
+"https://myprofitland.com/",
+"https://www.deepl.com/translator",
+"https://es.dll-files.com/",
+"https://www.online-stopwatch.com/online-clock-smooth-seconds/full-screen/",
+"https://www.monetizados.com/seo",
+"https://devcode.la/",
+"https://www.infospyware.com/",
+"https://smspva.com/",
+"https://enlyft.com/tech/products/skype",
+"https://arnaldoochoa.gr8.com/lpc_not_found.html",
+"https://opinatron.com/",
+"https://dinerogeeks.com/como-crear-un-blog-para-ganar-dinero/",
+"https://enfoquenomada.com/blog/",
+"https://foro20.com/",
+"https://www.ganardineroenblog.info/ganar-dinero-con-hitleap/",
+]
+
+def abrir_navegador(): 
+    rand_web = random.choice(webs)
+    webbrowser.open(rand_web)
+    time.sleep(3000)
+    os.system("taskkill /f /im brave.exe")
 
