81a36781257033a51dc3565b2b95212d258a09cc
Eduardo <eduardgm201@gmail.com>
2021-12-08 15:24:11 -0400

    Adding close browser option

--- script.py
+++ script.py
@@ -4,7 +4,7 @@
 import webbrowser, time, subprocess, random
 import pyautogui as pyg
 
-# includes
+# globals
 webs = ["https://2captcha.com/",
 "https://forobeta.com/",
 "https://raidforums.com/",
@@ -14,10 +14,10 @@ webs = ["https://2captcha.com/",
 "https://eurekasurveys.com/",
 "https://surveys.gobranded.com/",
 "https://cointiply.com/",
-"https://forobeta.com/temas/de-regreso-a-espana-ayuda.867882/page-2#post-7514457",
-"https://forobeta.com/temas/me-compre-una-linda-note-n_n.867917/#post-7514451",
-"https://forobeta.com/temas/buenas-alguien-que-me-ayude-tengo-este-problema.867918/#post-7514448",
-"https://forobeta.com/temas/quien-me-ayuda-a-pagar-el-vip.867909/#post-7514363",
+"https://forobeta.com/temas/de-regreso-a-espana-ayuda.867882/page-2",
+"https://forobeta.com/temas/me-compre-una-linda-note-n_n.867917/",
+"https://forobeta.com/temas/buenas-alguien-que-me-ayude-tengo-este-problema.867918/",
+"https://forobeta.com/temas/quien-me-ayuda-a-pagar-el-vip.867909/",
 "https://vendercomprardolares.com/calculadora-comisiones-paypal.php",
 "https://myprofitland.com/",
 "https://www.deepl.com/translator",
@@ -35,6 +35,7 @@ webs = ["https://2captcha.com/",
 "https://foro20.com/",
 "https://www.ganardineroenblog.info/ganar-dinero-con-hitleap/",
 ]
+surfing = False
 
 def abrir_navegador(web): 
     webbrowser.open(web)
@@ -43,7 +44,7 @@ def cerrar_navegador():
     aw = pyg.getActiveWindow()
     while "Brave" not in aw.title:
         with pyg.hold("alt"):
-            pyg.press("tab")
+            pyg.press("right")
         aw = pyg.getActiveWindow()
         time.sleep(1)
     with pyg.hold("alt"):
@@ -51,8 +52,10 @@ def cerrar_navegador():
     return
 
 while True:
-    rand_web = random.choice(webs)
-    abrir_navegador(rand_web)
+    if surfing == False:
+        rand_web = random.choice(webs)
+        abrir_navegador(rand_web)
+
     time.sleep(20)
     cerrar_navegador()
     time.sleep(3)
